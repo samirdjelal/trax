834c9ce01b94951b70f3a7c70125880fa4330cf8
Samir Djelal <samir.djelal.webdesign@gmail.com>
2022-05-24 19:59:04 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use dotenv::dotenv;
+use std::thread;
+use std::time::Duration;
+use threadpool::ThreadPool;
+
+fn main() -> Result<()> {
+    dotenv().ok();
+
+    let pool = ThreadPool::new(4);
+
+    let (sender, receiver) = std::sync::mpsc::channel();
+
+    for _ in 0..4 {
+        let sender = sender.clone();
+        pool.execute(move || {
+            thread::sleep(Duration::from_secs(1));
+            sender.send("Hello from thread pool").unwrap();
+        });
+    }
+
+    loop {
+        let msg = receiver.recv().unwrap();
+        println!("{}", msg);
+    }
+
+    // Ok(())
 }
